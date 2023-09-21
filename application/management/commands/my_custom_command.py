43b019e15eedcbca1f2bb7b682f15197cd7a9ffb
Ablaev.b <86608383+DrPepper01@users.noreply.github.com>
2023-09-21 11:23:59 +0600

    Команда для печати количества студентов в курса и количества курсов у студентов

--- application/management/commands/my_custom_command.py
+++ application/management/commands/my_custom_command.py
@@ -5,7 +5,30 @@ from application.models import Students, Courses
 
 
 class Command(BaseCommand):
-    help = u'Создание случайных студентов'
+
+    def handle(self, *args, **kwargs):
+        # Получаем список всех курсов
+        courses = Courses.objects.all()
+
+        # Выводим список студентов для каждого курса
+        for course in courses:
+            students = course.stud.all()
+            self.stdout.write(f"Студенты для курса '{course.title}':")
+            for student in students:
+                self.stdout.write(f"- {student.name}")
+
+        # Получаем список всех студентов
+        students = Students.objects.all()
+
+        # Выводим список курсов для каждого студента
+        for student in students:
+            courses = student.courses.all()
+            self.stdout.write(f"Курсы для студента '{student.name}':")
+            for course in courses:
+                self.stdout.write(f"- {course.title}")
+
+
+class NewCommand(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('total', type=int, help=u'Количество создаваемых Студентов и курсов')
